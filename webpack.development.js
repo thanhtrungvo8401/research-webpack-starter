958e4f7d330285df1ec359846a0b91c79ec0c36d
trungvt <thanhtrungvo8401@gmail.com>
2022-08-14 11:24:17 +0700

    master: create multiple bundle file

--- webpack.development.js
+++ webpack.development.js
@@ -2,9 +2,12 @@ const path = require("path");
 
 module.exports = {
     mode: "development",
-    entry: path.resolve(__dirname, 'src/index.js'),
+    entry: {
+        home: path.resolve(__dirname, 'src/index.js'),
+        home2: path.resolve(__dirname, 'src/index.js')
+    },
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: "bundle.js"
+        filename: '[name].js'
     }
 }
